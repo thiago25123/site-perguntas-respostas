7703c6c64ba5c2d146b942a8a033250340452262
thiago25123 <147066617+thiago25123@users.noreply.github.com>
2024-09-11 21:33:58 -0300

    Add files via upload

---
+++ Nova pasta (2)/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    "programação",
+    "Oque é JavaScript?",
+    "o JavaScript é uma linguagem de programação"
+)
+
+criaCartao(
+    "Geografia",
+    "Qual a capital da França?",
+    "A capital da França é Paris"
+)
+
+criaCartao(
+    "Programação",
+    "Oque é uma função?",
+    "Uma função é um bloco de código que executa alguma tarefa"
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz OI em inglês?',
+    'Oi em ingles é HI (RAI)'
+) 
